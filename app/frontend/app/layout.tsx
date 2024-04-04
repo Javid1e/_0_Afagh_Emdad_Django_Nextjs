c58e7e9086021af36b1a59545c2929deae5c213d
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-04-04 05:32:48 +0330

    Update layout.tsx
    + Define Viewport, ThemeColorSetting,CustomMetadata
    Types
    * Import Head From Next.js
    + Define rtl

--- app/frontend/app/layout.tsx
+++ app/frontend/app/layout.tsx
@@ -4,22 +4,44 @@ import { siteConfig } from '@/config/site';
 import { Providers } from './providers';
 import clsx from 'clsx';
 import React from 'react';
+import Head from 'next/head';
 
-export const metadata: Metadata = {
+interface Viewport {
+  width: string;
+  initialScale: number;
+  userScalable: boolean | undefined;
+}
+
+interface ThemeColorSetting {
+  media: string;
+  color: string;
+}
+
+interface CustomMetadata extends Metadata {
+  viewport?: Viewport;
+  themeColor?: ThemeColorSetting[];
+}
+
+export const metadata: CustomMetadata = {
   title: {
     default: siteConfig.name,
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
-  ],
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png',
   },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    userScalable: false,
+  },
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
 };
 
 export default function RootLayout({
@@ -28,8 +50,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
+    <html lang="en" dir="rtl" suppressHydrationWarning>
+      <Head>
+        {metadata.viewport && (
+          <meta
+            name="viewport"
+            content={`width=${metadata.viewport.width}, initial-scale=${metadata.viewport.initialScale},
+             user-scalable=${metadata.viewport.userScalable ? 'yes' : 'no'}`}
+          />
+        )}
+        {metadata.themeColor?.map((colorSetting, index) => (
+          <meta
+            key={index}
+            name="theme-color"
+            content={colorSetting.color}
+            media={colorSetting.media}
+          />
+        ))}
+      </Head>
       <body className={clsx(' bg-background font-sans antialiased')}>
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
           {children}
