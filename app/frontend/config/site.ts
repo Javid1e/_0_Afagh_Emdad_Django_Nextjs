22dd260d0854749d587664d876ade000729b0149
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-04-05 22:31:17 +0330

    Add Footer Links

--- app/frontend/config/site.ts
+++ app/frontend/config/site.ts
@@ -11,4 +11,76 @@ export const siteConfig = {
     { label: 'سوالات رایج', href: '#faq' },
     { label: 'تماس با ما', href: '#contactus' },
   ],
+  footerBrandLinks: [
+    { id: 'about-us', label: 'درباره ما', href: '#aboutus', target: '_blank' },
+    { id: 'services', label: 'خدمات', href: '#services', target: '_blank' },
+    { id: 'gallery', label: 'گالری', href: '#gallery', target: '_blank' },
+  ],
+
+  footerCustomerLinks: [
+    { id: 'faqs', label: 'پرسش پاسخ', href: '#faq', target: '_blank' },
+    { id: 'reviews', label: 'بازخورد ها', href: '#reviews', target: '_blank' },
+    { id: 'support', label: 'پشتیبانی', href: '/support', target: '_blank' },
+  ],
+
+  footerServiceLinks: [
+    { id: 'news', label: 'اخبار', href: '/news', target: '_blank' },
+    {
+      id: 'complaint',
+      label: 'ثبت شکایت',
+      href: '/complaint',
+      target: '_blank',
+    },
+    {
+      id: 'contact-us',
+      label: 'تماس با ما',
+      href: '#contactus',
+      target: '_blank',
+    },
+  ],
+
+  footerBlogLinks: [
+    { id: 'post-1', label: 'تعویض باتری', href: '#changeb', target: '_blank' },
+    { id: 'post-2', label: 'تعویض لنت', href: '#changel', target: '_blank' },
+    { id: 'blog', label: 'پست های بیشتر', href: '/blog', target: '_blank' },
+  ],
+  //TODO: Update social links
+  footerSocial: [
+    { label: 'RubikaIcon', href: '#rubika', name: 'روبیکا', target: '_blank' },
+    {
+      label: 'WhatsAppIcon',
+      href: '#whatsapp',
+      name: 'واتساپ',
+      target: '_blank',
+    },
+    {
+      label: 'InstagramIcon',
+      href: '#instagram',
+      name: 'اینستاگرام',
+      target: '_blank',
+    },
+    {
+      label: 'TelegramIcon',
+      href: '#telegram',
+      name: 'تلگرام',
+      target: '_blank',
+    },
+    { label: 'BaleIcon', href: '#bale', name: 'بله', target: '_blank' },
+  ],
+
+  footerSiteInfo: [
+    { id: 'terms', label: 'شرایط استفاده', href: '/terms', target: '_blank' },
+    {
+      id: 'developer',
+      label: 'سایت از مجموعه ی دانا',
+      href: 'https://www.danadev.com',
+      target: '_blank',
+    },
+    {
+      id: 'policies',
+      label: 'حریم خصوصی',
+      href: '/policies',
+      target: '_blank',
+    },
+  ],
 };
