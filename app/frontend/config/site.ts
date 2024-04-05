756a8304137db8e0b8c5acb1e9f4a3e9399eb951
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-04-05 17:36:48 +0330

    Define Navigation Items

--- app/frontend/config/site.ts
+++ app/frontend/config/site.ts
@@ -3,4 +3,12 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: 'امداد خودرو افاق',
   description: 'خدمات امدادی و یدک کش خودرو در استان اصفهان و جاده های حومه',
+  siteNavigationItems: [
+    { label: 'درباره ما', href: '#aboutus' },
+    { label: 'خدمات', href: '#services' },
+    { label: 'گالری', href: '#gallery' },
+    { label: 'نظرات مشتریان', href: '#reviews' },
+    { label: 'سوالات رایج', href: '#faq' },
+    { label: 'تماس با ما', href: '#contactus' },
+  ],
 };
