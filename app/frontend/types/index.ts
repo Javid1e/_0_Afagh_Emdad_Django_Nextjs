f76a58c4cb3163bb5c5e4ef1a96285c78f5aa19b
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-04-06 21:49:57 +0330

    Define Needed types

--- app/frontend/types/index.ts
+++ app/frontend/types/index.ts
@@ -1,5 +1,26 @@
-import { SVGProps } from 'react';
+import React, { SVGProps } from 'react';
 
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
+};
+export type IconComponentMap = Record<string, React.FC<IconSvgProps>>;
+export type CommonComponentProps = {
+  id?: string;
+  className?: string;
+};
+export type ActiveLinkProps = {
+  activeItem?: string;
+};
+export type InsideLinkProps = {
+  label: string;
+  href: string;
+};
+export type OutSideLinkProps = {
+  id: string;
+  label: string;
+  href: string;
+  target: string;
+};
+export type OutSideNamedLinkProps = OutSideLinkProps & {
+  name: string;
 };
