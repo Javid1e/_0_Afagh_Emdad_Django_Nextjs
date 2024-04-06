e396fe3f5f1ffaccdc3d17d507b3c7199db0f9df
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-04-06 21:43:59 +0330

    Define (Main) Route
    + Use Header & Footer In Here
    + Dynamic Rendering Items Pass To Header & Footer

---
+++ app/frontend/app/(Main)/_page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { AboutUs } from '@/components/Layouts/main/AboutUs';
+import Header from '@/components/Layouts/Header/Header';
+import { Footer } from '@/components/Layouts/Footer/Footer';
+import { siteConfig } from '@/config/site';
+
+export const RoutePage = () => {
+  return (
+    <div className="relative flex flex-col h-full">
+      <Header
+        id="landing-header-navbar"
+        headerActionLinks={siteConfig.headerActionsLinks}
+        headerNavigationLinks={siteConfig.landingHeaderNavigationLinks}
+        headerMenuLinks={siteConfig.landingHeaderMenuLinks}
+      />
+      <main className="container mx-auto pt-16 px-6 flex-grow">
+        <AboutUs
+          id="about-us"
+          className="drop-shadow-2xl flex flex-col items-stretch content-center flex-warp justify-space-evenly border-b-2 border-primary-600"
+        />
+      </main>
+      <Footer
+        id="landing-footer"
+        className="bg-background/70 shadow-xl rounded-t-lg backdrop-blur-xl backdrop-saturate-150 mt-16"
+        footerBrandLinks={siteConfig.footerBrandLinks}
+        footerCustomerLinks={siteConfig.footerCustomerLinks}
+        footerOtherServicesLinks={siteConfig.footerServiceLinks}
+        footerBlogLinks={siteConfig.footerBlogLinks}
+        footerSocialLinks={siteConfig.footerSocial}
+        footerSiteInfoLinks={siteConfig.footerSiteInfo}
+      />
+    </div>
+  );
+};
---
+++ app/frontend/app/(Main)/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { RoutePage } from '@/app/(Main)/_page';
+
+const page = async () => {
+  return <RoutePage />;
+};
+export default page;
