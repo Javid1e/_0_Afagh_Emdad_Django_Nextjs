d5647de8eb1439c38298557dfb7a0a353f06244a
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-04-06 21:44:38 +0330

    Define aboutus Route
    + Use Header & Footer In Here
    + Dynamic Rendering Items Pass To Header & Footer

---
+++ app/frontend/app/aboutus/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import AboutUs from '@/components/Routes/AboutUs/AboutUs';
+import Header from '@/components/Layouts/Header/Header';
+import { Footer } from '@/components/Layouts/Footer/Footer';
+import { siteConfig } from '@/config/site';
+
+const page = async () => {
+  return (
+    <div className="relative flex flex-col h-full">
+      <Header
+        headerActionLinks={siteConfig.headerActionsLinks}
+        headerNavigationLinks={siteConfig.headerNavigationLinks}
+        headerMenuLinks={siteConfig.headerMenuLinks}
+      />
+      <main className="container mx-auto pt-16 px-6 flex-grow">
+        <AboutUs />
+      </main>
+      <Footer
+        id="about-footer"
+        className="bg-background/70 shadow-xl rounded-t-lg backdrop-blur-xl backdrop-saturate-150 mt-16"
+        footerBrandLinks={siteConfig.footerBrandLinks}
+        footerCustomerLinks={siteConfig.footerCustomerLinks}
+        footerOtherServicesLinks={siteConfig.footerServiceLinks}
+        footerBlogLinks={siteConfig.footerBlogLinks}
+        footerSocialLinks={siteConfig.footerSocial}
+        footerSiteInfoLinks={siteConfig.footerSiteInfo}
+      />
+    </div>
+  );
+};
+export default page;
