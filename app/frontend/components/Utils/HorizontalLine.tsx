26e55fdd55e42e3c7c23a3649599301db1693842
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-04-17 18:33:47 +0330

    Create HorizontalLine.tsx

---
+++ app/frontend/components/Utils/HorizontalLine.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HorizontalLine = () => {
+  return (
+    <hr className=" mt-4 sm:mt-12 lg:mt-20 border-b-1.5 border-primary-600" />
+  );
+};
+
+export default HorizontalLine;
